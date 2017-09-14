bee19345c2d25040aa07f795c2b46c66e46cbc83
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2017-09-14 16:50:22 -0700

    include: Add helper to retrieve a __s64 from a netlink msg
    This allows signed 64-bit integers to be retrieved from a netlink
    message.

--- include/libnetlink.h
+++ include/libnetlink.h
@@ -185,6 +185,13 @@ static inline __u64 rta_getattr_u64(const struct rtattr *rta)
 	memcpy(&tmp, RTA_DATA(rta), sizeof(__u64));
 	return tmp;
 }
+static inline __s64 rta_getattr_s64(const struct rtattr *rta)
+{
+	__s64 tmp;
+
+	memcpy(&tmp, RTA_DATA(rta), sizeof(__s64));
+	return tmp;
+}
 static inline const char *rta_getattr_str(const struct rtattr *rta)
 {
 	return (const char *)RTA_DATA(rta);
